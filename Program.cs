9f6bb8dcbeec99457b4ad7d44fb00ebcc3b185e0
Luisa Farias De Macedo <luisa.macedo@ndd.tech>
2024-05-27 21:26:29 -0300

    feat: arquivos do projeto para o desafio de orientação a objeto, implementando conceitos como abstração, herança e orientação a objeto

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Nokia");
+Smartphone nokia = new Nokia(numero: "232423", modelo: "modelo", imei: "imei", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Iphone");
+Smartphone iphone = new Iphone(numero: "231321", modelo: "modelo 2", imei: "imei", memoria: 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Zoom");
